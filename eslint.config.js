fd3b336de7c9b29c3ad994376b3a8a61167814cd
aashish <ritikkamboj4314@gmail.com>
2025-12-17 20:19:17 +0530

    added eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   {
@@ -10,21 +11,26 @@ export default [
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: "module",
-        project: "./tsconfig.json"
-      }
+        project: "./tsconfig.json",
+      },
     },
 
     plugins: {
-      "@typescript-eslint": tsPlugin
+      "@typescript-eslint": tsPlugin,
+      "simple-import-sort": simpleImportSort,
     },
 
     rules: {
       "no-var": "error",
       "prefer-const": "error",
-      "semi": ["error", "always"],
+      semi: ["error", "always"],
       "no-multi-spaces": "error",
       "space-in-parens": "error",
-      "no-multiple-empty-lines": "error"
-    }
-  }
+      "no-multiple-empty-lines": "error",
+      "prefer-const": "error",
+      "no-unused-vars": ["warn"],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
