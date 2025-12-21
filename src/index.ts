f206278b61d0d710755873fc3e0255e8dd098340
aashish <ritikkamboj4314@gmail.com>
2025-12-21 17:59:35 +0530

    integerated bull Board UI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { Express } from "express";
 import express from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig.js";
 import serverConfig from "./config/serverConfig.js";
 import sampleQueueProducer from "./producer/sampleQueueProducer.js";
 import apiRouter from "./router/index.js";
@@ -9,12 +10,14 @@ import SampleWorker from "./workers/sampleWorker.js";
 const app: Express = express();
 
 app.use("/api", apiRouter);
+app.use('/ui',bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   const a = 10;
   console.log(a);
 
   console.log(`server is running  or well  on ${serverConfig.PORT}`);
+  console.log(`bull board dashboard running on server : http://localhost:${serverConfig.PORT}/ui`);
 
   SampleWorker("SampleQueue");
 
